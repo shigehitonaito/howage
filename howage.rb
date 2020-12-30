8216779078c039853c0ba709d036f3175571f1f7
shigehitonaito <shigehito.7110.330@gmail.com>
2020-12-30 09:36:04 +0000

    first commit

---
+++ howage.rb
@@ -0,0 +1,44 @@
+
+
+
+class Guest
+	attr_accessor :age
+
+	def initialize(age = 50)
+		@age = age
+	end
+
+	def set_age(answer)
+		case answer
+		when "true"
+			puts "あなたの年齢は#{@age}です!!"
+		when "up"
+			@age += 1
+			self.answer
+		when "down"
+			@age -= 1
+			self.answer
+		end
+	end
+
+	def answer
+		puts "あなたの年齢は#{@age}ですか？"
+		set_age(answer = gets.chomp)
+	end
+end
+
+puts text = <<~TEXT
+こんにちわ！
+これからの質問にtrueかupかdownで答えてください。
+TEXT
+
+guest = Guest.new
+guest.answer
+
+
+
+
+
+
+
+
